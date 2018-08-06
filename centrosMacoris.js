cd0ecfb5c6a5edea72fe2c3f80afd11b18a1497d
Renys <ronal2w@gmail.com>
2018-08-05 23:57:35 -0400

    UI de agregar doctor agregado

---
+++ centrosMacoris.js
@@ -0,0 +1,197 @@
+import React, {Component} from 'react'
+import {Image, ScrollView, Text, AsyncStorage, Linking} from 'react-native'
+import { Card, CardItem, Body, Left, Button, Icon, Right, Spinner} from 'native-base'
+import {createStackNavigator} from 'react-navigation'
+import Comments from './commentsCentros'
+
+const GenerateCards = (obj) =>
+{
+    const navigate = obj.navigate
+    var thisliked = false    
+    if(obj.centros.length>0)
+    {
+        return obj.centros.map((v,i) => {
+            thisliked=false;        
+            if(obj.liked != undefined)
+            {
+                for(let x=0; x<obj.liked.length;x++)
+                {
+                    // alert(obj.liked[x].idcentro + " - " + v.id)
+                    if(obj.liked[x].idcentro == v.id)
+                    {
+                        thisliked = true;
+                        break;
+                    }
+                }
+            }                  
+            return <Card key={i} >
+            <CardItem>
+                <Body>
+                    <Text>{v.nombre}</Text>
+                    <Text note>{v.ciudad + ", " + v.direccion}</Text>
+                </Body>
+            </CardItem>
+            <CardItem cardBody>
+                <Image source={{uri: v.image}} style={{height:200, width:200, flex:1}} />
+            </CardItem>
+            <CardItem>
+                <Left>
+                    <Button transparent onPress={() => obj.handleLike(v.id, thisliked)}  >
+                        <Icon  style={thisliked?"":{color: 'gray'}} name="thumbs-up"/>
+                        <Text>{" " + v.likes + " Likes"}</Text>
+                    </Button>
+                </Left>
+                <Body>
+                    <Button transparent onPress={() => navigate.push("Commentx", {idCentro: v.id})} >
+                        <Icon style={{color: "gray"}} name="chatbubbles"/>
+                        <Text>Comentarios</Text>
+                    </Button>
+                </Body>
+                <Right>
+                    <Button transparent onPress={() => Linking.openURL("https://www.google.com/maps/search/?api=1&query="+v.nombre+"+Hospital")} >
+                        <Icon style={{color: "gray"}} name="map"/>
+                        <Text> Cómo llegar</Text>
+                    </Button>
+                </Right>
+            </CardItem>
+        </Card>
+        })
+    }
+    return <Spinner/>
+}
+class Centros extends Component
+{
+    state = 
+    {
+        liked: [{idcentro: 'a'}],
+        likes: [126,133,12,89],
+        city: "", centros:[], actualUser:""
+    }
+
+    componentDidMount()
+    {        
+        // const city = this.props.navigation.getParam("ciudad");
+        // alert(city)
+        this.getCentrosCiudad("Macoris");     
+        this.getMisLikes();
+        AsyncStorage.getItem("username", (err, result) => {
+            if(!err)
+            {
+                // alert(result)
+                this.setState({actualUser: result});
+                fetch("https://serverquedoctor.herokuapp.com/usuarioActual?usuario="+result);
+            }
+        })
+    }
+
+    
+
+    getMisLikes = () =>
+    {
+        this.fetchMisLikes()
+            .then(res => this.setState({liked: res}))
+            .catch(err => console.log(err));
+    }
+    
+    fetchMisLikes = async() => {
+        const response = await fetch("https://serverquedoctor.herokuapp.com/centros/misLikes");
+        const body = response.json();
+        if(response.status != 200) throw Error(body.message)
+        return body;
+    }
+
+    fetchCentrosCiudad = async (city) => {
+        const response = await fetch("https://serverquedoctor.herokuapp.com/centros/ciudad?ciudad="+city);
+        const body = response.json();
+        if(response.status != 200) throw Error(body.message)
+        return body;
+    }
+
+    getCentrosCiudad = (city) => {
+        this.fetchCentrosCiudad(city)
+            .then(res => this.setState({centros: res}))
+            .catch(err => console.log(err));
+    }
+
+    fetchLike = async(idCentro) =>
+    {
+        const response = await fetch("https://serverquedoctor.herokuapp.com/centros/like?idCentro="+idCentro);
+        const body = response.json();
+        if(response.status != 200) throw Error(body.message)
+        return body;
+    }
+    fetchUnLike = async(idCentro) =>
+    {
+        const response = await fetch("https://serverquedoctor.herokuapp.com/centros/unlike?idCentro="+idCentro);
+        const body = response.json();
+        if(response.status != 200) throw Error(body.message)
+        return body;
+    }
+
+    handleLike = (idCentro, liked) => {
+        // alert(numHospital)
+        // if(!liked)
+        // {
+        //     this.fetchLike(idCentro)
+        //         .then(res => {
+        //             // this.setState({centros: res})
+        //             this.getCentrosCiudad("Santiago De Los Caballeros");                         
+        //             this.getMisLikes();
+        //         })
+        //         .catch(err => console.log(err));
+                
+            
+        // } else {
+        //     this.fetchUnLike(idCentro)
+        //         .then(res => {
+        //             // this.setState({centros: res})
+        //             this.getCentrosCiudad("Santiago De Los Caballeros");
+        //             this.getMisLikes();
+        //         })
+        //         .catch(err => console.log(err));
+        //         this.getMisLikes();
+        //     }
+        this.fetchLike(idCentro)
+            .then(res => {
+                // this.setState({centros: res})
+                this.getCentrosCiudad("Macoris");
+                this.getMisLikes();
+            })
+            .catch(err => console.log(err));
+        
+    }
+
+    static navigationOptions = {
+        header: null
+        // title: "asdaasdsa"
+    }
+
+    render()
+    {
+        const {centros, liked, likes} = this.state;
+        
+        // console.warn(liked)
+        return(
+            <ScrollView>
+                <GenerateCards centros={centros} navigate={this.props.navigation} handleLike={this.handleLike} liked={liked} likes={likes} />
+            </ScrollView>
+        )
+    }
+}
+//Si pongo esta vaina entonces no me sirve getParam, pero si no lo pongo no me sirve el Commentx :D
+const RootStack = createStackNavigator(
+    {
+        Home: Centros,
+        Commentx: Comments,
+    },
+    {
+        initialRouteName: "Home",
+        initialRouteParams: {city:"si"}
+    }
+)
+
+export default class App extends Component {    
+    render() {        
+        return <RootStack/>
+    }
+}
---
+++ doctores.js
@@ -0,0 +1,275 @@
+import React, { Component } from 'react'
+import { ScrollView, StyleSheet, Text, AsyncStorage } from 'react-native'
+import { Card, CardItem, Picker, Item, Icon, Fab, Textarea, View, Form, Content, Button, Body, List, ListItem, Left, Thumbnail, Right, Spinner, Container, Input } from 'native-base'
+import { Avatar, Divider } from 'react-native-elements'
+import moment from 'moment'
+import Modal from "react-native-modal";
+import {createStackNavigator} from 'react-navigation'
+
+
+const AddDoctor = (obj) => {
+    return <View style={styles.modalContent}>
+
+        <Text style={{ textAlign: "center" }} >¿Quién fue tú Doctor?</Text>
+        <Item>
+            <Input placeholder="Nombre del doctor"/>
+        </Item>
+        <Item>
+            <Input placeholder="Apellidos"/>
+        </Item>
+        <Item picker>
+            <Picker
+                mode="dropdown"
+                placeholder="Seleccionar"
+                iosIcon={<Icon name="ios-arrow-down-outline" />}
+                placeholderStyle={{ color: "#bfc6ea" }}
+                placeholderIconColor="#007aff"
+                selectedValue={obj.valueNombre}
+                onValueChange={obj.handlePickerNombre}
+            >
+                {obj.especialidades}
+            </Picker>
+        </Item>
+        <Item picker>
+                <Picker
+
+                    mode="dropdown"
+                    placeholder="Seleccionar"
+                    iosIcon={<Icon name="ios-arrow-down-outline" />}
+                    placeholderStyle={{ color: "#bfc6ea" }}
+                    placeholderIconColor="#007aff"
+                    selectedValue={obj.valueServicio}
+                    onValueChange={obj.handlePickerNombre}
+                >
+                    <Picker.Item label="¿Que tal fue la experiencia?" value="0" />
+                    <Picker.Item label="Recomendado" value="recomendado" />
+                    <Picker.Item label="Aceptable" value="aceptable" />
+                    <Picker.Item label="No recomendado" value="noRecomendado" />
+                </Picker>
+            </Item>
+        
+        {/* <Content> */}
+        <Form>
+            <Textarea rowSpan={5} value={obj.comment} onChangeText={(text) => obj.handleComment(text)} placeholder="Cuéntanos un poquito más" />
+            <Button block style={{backgroundColor:'#03a9f4'}} onPress={obj.enviarComentario} ><Text style={{ color: "white" }} >Enviar</Text></Button>
+        </Form>
+    </View>
+}
+
+
+const Doctores = (obj) => {
+    var numColor = 0;
+    var firstLetterUserName;
+    // console.warn(obj.comments)
+    // alert(obj.comments)
+    // alert(obj.loading)
+    // alert(obj.loading + "-" +obj.comments.length)
+    if (!obj.loading) {
+        if(obj.comments.length >0)
+        {
+            return obj.comments.map((v, i) => {
+                numColor = Math.floor(Math.random() * (5 - 0) + 0)
+                // firstLetterUserName = v.usuario.toUpperCase();
+                // alert(numColor)
+                return <List key={i} on >
+                    <ListItem avatar >
+                        <Left>
+                            {/* <Thumbnail source={{uri: ""}}/> */}
+                            <Avatar size="small" rounded title={v.usuario != undefined ? v.usuario[0].toUpperCase() : ""} activeOpacity={0.7} overlayContainerStyle={{ backgroundColor: obj.colors[numColor] }} />
+                        </Left>
+                        <Body>
+                            <Text style={{ fontWeight: "bold" }} >{v.usuario}</Text>
+                            <Text>{v.recomendado.toUpperCase() + " - " + v.comentario}</Text>
+                            {obj.actualUser == v.usuario ?
+                                <Button block transparent onPress={() => obj.deleteComment(v.id)}><Text>Borrar comentario</Text></Button>
+                                : <Text />}
+                        </Body>
+                        <Right>
+                            <Text note>{v.fecha}</Text>
+                        </Right>
+                    </ListItem>
+                </List>
+            })
+        } else {
+            return <View  style={{flex:1, flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+
+            <Text  >Sin doctores</Text>
+            </View>
+        }
+        
+    } else {
+        return <Spinner />
+    }
+    
+    
+    
+}
+
+class classComments extends Component {
+    state =
+        {
+            valueServicio: "0", valueNombre: "0", date: moment(new Date()).format("DD/MM/YYYY"), comment: "",
+            avatarColors: ["#d32f2f", "#7b1fa2", "#1976d2", "#388e3c", "#ffa000", "#e64a19"],
+            comments: [], username: "", idCentro: "", modal:false, loading:true
+        }
+    handlePickerServicio = (value: string) => {
+        this.setState({ valueServicio: value })
+    }
+    handlePickerNombre = (value: string) => {
+        this.setState({ valueNombre: value })
+    }
+
+    componentDidMount() {
+        var idCentro = this.props.navigation.getParam("idCentro");
+        this.setState({ idCentro })
+        // alert(idCentro)
+        AsyncStorage.getItem("username", (err, result) => {
+            if (!err) {
+                this.setState({ username: result })
+            } else {
+                console.error(err)
+            }
+        })
+
+        this.getComentarios(idCentro);
+
+    }
+
+    deleteComment = (idComment) => {
+        const { idCentro } = this.state;
+        // alert(idComment)
+        this.fetchDeleteComment(idComment, idCentro)
+            .then(res => this.setState({ comments: res }))
+            .catch(err => {
+                console.log("Error borrando comentario: " + err);
+                alert("Ha ocurrido un error eliminando el comentario")
+            })
+    }
+
+    fetchDeleteComment = async (idComment, idCentro) => {
+        const response = await fetch("https://serverquedoctor.herokuapp.com/comentarios/centro/eliminar?id=" + idComment + "&fkCentro=" + idCentro);
+        const body = response.json();
+        if (response.status != 200) throw Error(body.message)
+        return body;
+    }
+    // const {usuario, fecha, comentario, fkCentro} = req.query;
+    fetchAgregar = async () => {
+        const { valueServicio, valueNombre, date, comment, idCentro } = this.state;
+        // alert("x")
+        const response = await fetch("https://serverquedoctor.herokuapp.com/comentarios/centro/agregar?fkCentro=" + idCentro + "&fecha=" + date + "&comentario=" + comment + "&usuario=" + valueNombre + "&recomendado=" + valueServicio);
+        const body = response.json();
+        if (response.status != 200) throw Error(body.message)
+        return body;
+    }
+
+    getComentarios = (idCentro) => {
+        this.fetchComentarios(idCentro)
+            .then(res => this.setState({ comments: res, loading:false }))
+            .catch(err => {
+                alert("Error obteniendo comentarios")
+                console.log(err)
+            })
+    }
+
+    fetchComentarios = async (idCentro) => {
+        const response = await fetch("https://serverquedoctor.herokuapp.com/comentarios/centros?idCentro=" + idCentro);
+        const body = response.json();
+        if (response.status != 200) throw Error(body.message)
+        return body;
+    }
+
+    agregarComentario = (idCentro) => {
+        this.fetchAgregar(idCentro)
+            .then(res => {
+                this.setState({ comments: res, comment: "", valueServicio: "0", modal: false })
+            })
+            .catch(err => {
+                console.log(err);
+                alert("Error obteniendo nuevos comentarios")
+            })
+    }
+
+    handleComment = (text) => {
+        this.setState({ comment: text })
+    }
+
+    static navigationOptions = {
+        header: null
+    }
+
+    especialidades = (especialidades) =>
+    {
+        // return [1,2].map((v,i) => {
+           return especialidades.map((v,i) => {
+
+               return <Picker.Item label={v} value={v} />
+           })
+                
+        // })
+    }
+
+    render() {
+        const { valueServicio, valueNombre, comment, avatarColors, username, comments, modal, loading } = this.state
+        const especialidades = ["Seleccionar especialidad","Pediatra"]
+        // console.warn(comments)
+        return (
+            <Container style={{ backgroundColor: "white" }}>
+                 <ScrollView  >
+                
+                     {/* <AddDoctor comment={comment} handleComment={this.handleComment} enviarComentario={this.agregarComentario} handlePickerNombre={this.handlePickerNombre} handlePickerServicio={this.handlePickerServicio}
+                         valueServicio={valueServicio} valueNombre={valueNombre} username={username} /> */}
+                     <Divider />
+                     <Doctores loading={loading} actualUser={username} deleteComment={this.deleteComment} colors={avatarColors} comments={comments} />
+                 </ScrollView>
+                <View style={{flex: 1}} >
+                <Fab
+                    active={this.state.active}
+                    //#03a9f4
+                    containerStyle={{}}
+                    style={{ backgroundColor: '#03a9f4' }}
+                    position="bottomRight"
+                    onPress={() => this.setState({ modal:true })}>
+                    <Icon  type="MaterialCommunityIcons" name="comment-plus-outline"  />
+
+                </Fab>
+                </View>
+                <Modal
+          isVisible={modal}
+          animationIn="slideInLeft"
+          animationOut="slideOutRight"
+          onBackdropPress={() => this.setState({ modal: false })}
+        >
+          <AddDoctor especialidades={this.especialidades(especialidades)} comment={comment} handleComment={this.handleComment} enviarComentario={this.agregarComentario} handlePickerNombre={this.handlePickerNombre} handlePickerServicio={this.handlePickerServicio}
+                         valueServicio={valueServicio} valueNombre={valueNombre} username={username} />
+        </Modal>
+            </Container>            
+        )
+    }
+}
+const RootStack = createStackNavigator(
+    {
+        Home: classComments,
+        // Commentx: Comments,
+    },
+    {
+        initialRouteName: "Home",
+    }
+)
+
+const styles = StyleSheet.create({
+  modalContent: {
+    
+    backgroundColor: "white",
+    padding: 22,
+    // justifyContent: "center",
+    // alignItems: "center",
+    borderRadius: 4,
+    // borderColor: "rgba(0, 0, 0, 0.1)"
+  },
+})
+
+export default class App extends Component {    
+    render() {        
+        return <RootStack/>
+    }
+}
